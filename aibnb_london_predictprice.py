0d90b1614717c6200778c8296fee1877f5e94890
nicknaul <nickocnaul@gmail.com>
2024-09-24 13:54:37 +0800

    Update aibnb_london_predictprice.py

--- aibnb_london_predictprice.py
+++ aibnb_london_predictprice.py
@@ -9,7 +9,7 @@ import pickle
 def make_prediction():
     df_london_predict = pd.read_csv("X_test_london_final.csv", encoding='latin-1')
     df_london_predict=df_london_predict.drop(columns='Unnamed: 0')
-    with open("model-london-test.pkl", "rb") as f:
+    with open("model-london-airbnb.pkl", "rb") as f:
         model = pickle.load(f)
     predict_price = pd.Series(model.predict(df_london_predict))
 
